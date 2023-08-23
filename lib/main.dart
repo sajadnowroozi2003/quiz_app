c2141b6a8d7b7162c8fcf108e961ed4599575b8f
Sajad Nowroozi <133590385+sajadnowroozi2003@users.noreply.github.com>
2023-08-23 15:34:24 +0430

    create pase page

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,51 @@ class _QuizPageState extends State<QuizPage> {
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[],
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Center(
+              child: Text(
+                'This is where thr question text will appear',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.green,
+                  textStyle: TextStyle(fontSize: 20)),
+              child: Text('True'),
+              onPressed: () {
+                //the user clicked true
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.red,
+                  textStyle: TextStyle(fontSize: 20)),
+              child: Text('False'),
+              onPressed: () {
+                //the user clicked false
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
